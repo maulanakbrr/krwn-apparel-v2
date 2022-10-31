95548108f942464ffceea91560a38475b39e41d7
Akbar Maulana <amaulanaa995@gmail.com>
2022-10-31 17:22:19 +0700

    installed sass, updated App.jsx. created category-item and directory components and their styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Directory from "./components/directory/directory.component";
 
 function App() {
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ]
+
   return (
-    <div>Hello World</div>
+    <Directory categories={categories}/>
   );
 }
 
